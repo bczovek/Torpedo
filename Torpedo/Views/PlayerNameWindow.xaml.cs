045870998a51c34c8bd1128ce8be5d06965d1f8d
Balazs Takacs <takibali56@gmail.com>
2022-01-09 17:59:40 +0100

    Updating in player name window

--- Torpedo/Views/PlayerNameWindow.xaml.cs
+++ Torpedo/Views/PlayerNameWindow.xaml.cs
@@ -13,11 +13,11 @@ using System.Windows.Shapes;
 namespace Torpedo.Views
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for PlayerNameWindow.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class PlayerNameWindow : Window
     {
-        public Window1()
+        public PlayerNameWindow()
         {
             InitializeComponent();
         }
