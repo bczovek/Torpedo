c102fe973d8e80e2f1f351fbc349531c3bd1553d
Balazs Takacs <takibali56@gmail.com>
2022-01-12 19:02:11 +0100

    Adding PlayerNameViewModel file

---
+++ Torpedo/ViewModel/PlayerNameViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Torpedo.ViewModel
+{
+    class PlayerNameViewModel : INotifyPropertyChanged
+    {
+
+        public PlayerNameViewModel()
+        {
+            Start = new DelegateCommand(OnStart);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _twoPlayerGame;
+        public DelegateCommand Start { get; set; }
+
+        public bool TwoPlayerGame
+        {
+            get => _twoPlayerGame;
+            set
+            {
+                _twoPlayerGame = value;
+                NotifyChange(nameof(TwoPlayerGame));
+            }
+        }
+
+        private void OnStart()
+        {
+        }
+
+        private void NotifyChange(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- Torpedo/Views/GameWindow.xaml.cs
+++ Torpedo/Views/GameWindow.xaml.cs
@@ -21,38 +21,7 @@ namespace Torpedo.Views
         public GameWindow()
         {
             InitializeComponent();
-            InitTables();
         }
 
-        private void InitTables()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    var shape = new Rectangle();
-                    shape.Fill = Brushes.Black;
-                    var unitY = 50;
-                    var unitX = 50;
-                    shape.Width = unitY;
-                    shape.Height = unitX;
-                    Canvas.SetTop(shape, unitY * i + (i+1) * 5);
-                    Canvas.SetLeft(shape, unitX * j + (j+1) * 5);
-                    canvas.Children.Add(shape);
-                }
-                for (int j = 0; j < 10; j++)
-                {
-                    var shape = new Rectangle();
-                    shape.Fill = Brushes.Black;
-                    var unitY = 50;
-                    var unitX = 50;
-                    shape.Width = unitY;
-                    shape.Height = unitX;
-                    Canvas.SetTop(shape, unitY * i + (i + 1) * 5);
-                    Canvas.SetLeft(shape, unitX * j + (j + 1) * 5);
-                    canvas2.Children.Add(shape);
-                }
-            }
-        }
     }
 }
