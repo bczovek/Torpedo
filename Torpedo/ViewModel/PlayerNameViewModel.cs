0d0ace66e3407fac32fb8096902f0a6cd786045a
Balazs Takacs <takibali56@gmail.com>
2022-01-12 19:25:18 +0100

    Updating PlayerNameViewModel file

--- Torpedo/ViewModel/PlayerNameViewModel.cs
+++ Torpedo/ViewModel/PlayerNameViewModel.cs
@@ -2,13 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 
 namespace Torpedo.ViewModel
 {
     class PlayerNameViewModel : INotifyPropertyChanged
     {
-
         public PlayerNameViewModel()
         {
             Start = new DelegateCommand(OnStart);
@@ -16,7 +16,20 @@ namespace Torpedo.ViewModel
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private string _firstPlayer;
+        private string _secondPlayer;
         private bool _twoPlayerGame;
+
+        public string FirstPlayer { 
+            get => _firstPlayer;
+            set => _firstPlayer = value; }
+
+        public string SecondPlayer
+        {
+            get => _secondPlayer;
+            set => _secondPlayer = value;
+        }
+
         public DelegateCommand Start { get; set; }
 
         public bool TwoPlayerGame
@@ -31,6 +44,20 @@ namespace Torpedo.ViewModel
 
         private void OnStart()
         {
+            if (_twoPlayerGame)
+            {
+                if(!(string.IsNullOrWhiteSpace(_firstPlayer) || string.IsNullOrWhiteSpace(_secondPlayer)))
+                {
+
+                }
+            }
+            else
+            {
+                if(!string.IsNullOrWhiteSpace(_firstPlayer))
+                {
+
+                }
+            }
         }
 
         private void NotifyChange(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
